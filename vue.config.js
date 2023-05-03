7194ec53dbbbc4d9e3e4fee52178c4e0e5f63505
Cristhian Tapiero <cristapi56@gmail.com>
2023-05-03 17:57:24 -0500

    Actualizando vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/PatronDiseno/': '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/PatronDiseno/': '/',
+  baseUrl: '/PatronDiseno/'
 }
